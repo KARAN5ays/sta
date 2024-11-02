bdb6b038a3294501ffe0399224d63c1a3b68dca5
KARAN5ays <karanpokhrel999@gmail.com>
2024-11-02 18:39:44 +0545

    Add files via upload

---
+++ style.js
@@ -0,0 +1,14 @@
+ // Open the login form popup
+ function openLoginForm() {
+    document.getElementById("loginPopup").style.display = "flex";
+}
+
+// Close the login form popup
+function closeLoginForm() {
+    document.getElementById("loginPopup").style.display = "none";
+}
+
+// Function for Create Account (optional for further development)
+function createAccount() {
+    alert("Redirecting to Create Account page...");
+}
